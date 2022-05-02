9375de70d36d0dfe5eecb485f5c6d0fc4bb0553c
Dmitriy24S <devilins24@gmail.com>
2022-05-02 08:40:22 +0300

    feat: :sparkles: add loading message on fetch, max width adjust main/header, country cards spacing desktop

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ type CountryData = {
 function App() {
   const [data, setData] = useState<CountryData[]>([]);
   const [darkMode, setDarkMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Fetch data on load
   useEffect(() => {
@@ -33,10 +34,10 @@ function App() {
         const data = await response.json();
         // console.log(data);
         setData(data);
-        // setIsLoading(false);
+        setIsLoading(false);
         // setFilteredData(data);
       } catch (error) {
-        // setIsLoading(true);
+        setIsLoading(true);
         console.error(error);
       }
     };
@@ -53,6 +54,12 @@ function App() {
         </section>
         {/* Country list container*/}
         <section className="country-list">
+          {/* Loading */}
+          {isLoading && (
+            <section className="loading">
+              <h2>Loading...</h2>
+            </section>
+          )}
           {/* Country list */}
           {data.map((item) => (
             <CountryCard {...item} key={item.numericCode} />
